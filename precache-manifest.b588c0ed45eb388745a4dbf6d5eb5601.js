b5754d57095f03105992291509148930a5d734ad
nick adams <thebigdeal916@icloud.com>
2020-05-12 19:21:56 -0700

    Updates

---
+++ precache-manifest.b588c0ed45eb388745a4dbf6d5eb5601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6482a79f2dea471acabfe7db47a9f5f6",
+    "url": "/react-homework/index.html"
+  },
+  {
+    "revision": "2952580bf21c606ad14b",
+    "url": "/react-homework/static/css/main.c50e423a.chunk.css"
+  },
+  {
+    "revision": "a971c53e01734798a991",
+    "url": "/react-homework/static/js/2.2d6b6474.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-homework/static/js/2.2d6b6474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2952580bf21c606ad14b",
+    "url": "/react-homework/static/js/main.078e5af0.chunk.js"
+  },
+  {
+    "revision": "f0eb17d3b2edb306b744",
+    "url": "/react-homework/static/js/runtime-main.2221d40c.js"
+  }
+]);
